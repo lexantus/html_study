3602cbdd323eee5840a5f542ba5c4722ede38782
Alexey Rozhin <alexey.rozhin90@gmail.com>
2015-10-16 16:56:22 +0300

    luxoft_cv pdf

--- plan/scripts/plan_builder160915.js
+++ plan/scripts/plan_builder160915.js
@@ -3,6 +3,7 @@ addPlan("Machine learning MIT", null, true);
 addPlan("JavaScript autogenerate plan html", null, true);
 addPlan("Box2D", null);
 addPlan("Plan", null, false);
+addPlan("English", null, true);
 addPlan("Скороговорка");
 var thought = "Пора осуществить свою мечту и написать сетевые шахматы с искусственным интеллектом.";
 addThought(thought);
